bb292c2044b7f811a2bb834a8b35d4db1cd7f35d
[AndreaS] <solfa_90@hotmail.it>
2023-04-18 10:21:43 +0200

    base structure and pages

---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+import Homepage from "./Homepage/Homepage";
+import NotFound from "./NotFound/NotFound";
+import Tasks from "./Tasks/Tasks";
+import TasksLists from "./TasksLists/TasksLists";
+
+export { Homepage, NotFound, Tasks, TasksLists };
