45763d7aaf8caecb0e0fd29d19fed97b1bdc0729
[AndreaS] <solfa_90@hotmail.it>
2023-04-18 18:16:00 +0200

    fix, add navbar

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { Navbar } from "../../molecules";
 
 const Header = () => {
-  return <Container as="header">Header</Container>;
+  return (
+    <Container as="header" className="min-h">
+      <Navbar />
+    </Container>
+  );
 };
 
 export default Header;
