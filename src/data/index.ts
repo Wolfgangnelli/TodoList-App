e68f2b36585fd27bd6cde343aefed0179c313b5c
Andrea Solfanelli <andrea.solfanelli@webranking.it>
2023-04-19 23:26:44 +0200

    fake data

---
+++ src/data/index.ts
@@ -0,0 +1,57 @@
+import { TasksList } from '../utils/types';
+
+export const TasksLists: TasksList = {
+    data: [
+      {
+        title: "Trip to Paris",
+        id: 1,
+        tasks: [
+          { id: 1, name: "Book flight", description: "scopo, obiettivi, passaggi", priority: 1, inCharge: "Andrea", status: "pending, in progress, done" },
+          { id: 2, name: "Passport check", description: "scopo, obiettivi, passaggi", priority: 1, inCharge: "Andrea", status: "pending, in progress, done" },
+          { id: 3, name: "Hotel reservation", description: "scopo, obiettivi, passaggi", priority: 1, inCharge: "Andrea", status: "pending, in progress, done" },
+          { id: 4, name: "Book flight", description: "scopo, obiettivi, passaggi", priority: 1, inCharge: "Andrea", status: "pending, in progress, done" },
+          { id: 5, name: "Passport check", description: "scopo, obiettivi, passaggi", priority: 1, inCharge: "Andrea", status: "pending, in progress, done" },
+          { id: 6, name: "Hotel reservation", description: "scopo, obiettivi, passaggi", priority: 1, inCharge: "Andrea", status: "pending, in progress, done" },
+        ],
+      },
+      {
+        title: "Trip to Brasil",
+        id: 2,
+        tasks: [
+          { id: 1, name: "Book flight", description: "scopo, obiettivi, passaggi", priority: 1, inCharge: "Andrea", status: "pending" },
+          { id: 2, name: "Passport check", description: "scopo, obiettivi, passaggi", priority: 1, inCharge: "Andrea", status: "progress" },
+          { id: 3, name: "Hotel reservation", description: "scopo, obiettivi, passaggi", priority: 1, inCharge: "Andrea", status: "done" },
+          { id: 4, name: "Book flight", description: "scopo, obiettivi, passaggi", priority: 1, inCharge: "Andrea", status: "pending" },
+          { id: 5, name: "Passport check", description: "scopo, obiettivi, passaggi", priority: 1, inCharge: "Andrea", status: "progress" },
+          { id: 6, name: "Hotel reservation", description: "scopo, obiettivi, passaggi", priority: 1, inCharge: "Andrea", status: "pending" },
+        ],
+      },
+      {
+        title: "Trip to Italy",
+        id: 3,
+        tasks: [
+          { id: 1, name: "Book flight", description: "scopo, obiettivi, passaggi", priority: 1, inCharge: "Andrea", status: "pending, in progress, done" },
+          { id: 2, name: "Passport check", description: "scopo, obiettivi, passaggi", priority: 1, inCharge: "Andrea", status: "pending, in progress, done" },
+          { id: 3, name: "Hotel reservation", description: "scopo, obiettivi, passaggi", priority: 1, inCharge: "Andrea", status: "pending, in progress, done" },
+        ],
+      },
+      {
+        title: "Trip to Spain",
+        id: 4,
+        tasks: [
+          { id: 1, name: "Book flight", description: "scopo, obiettivi, passaggi", priority: 1, inCharge: "Andrea", status: "pending, in progress, done" },
+          { id: 2, name: "Passport check", description: "scopo, obiettivi, passaggi", priority: 1, inCharge: "Andrea", status: "pending, in progress, done" },
+          { id: 3, name: "Hotel reservation", description: "scopo, obiettivi, passaggi", priority: 1, inCharge: "Andrea", status: "pending, in progress, done" },
+        ],
+      },
+      {
+        title: "Trip to Germany",
+        id: 5,
+        tasks: [
+          { id: 1, name: "Book flight", description: "scopo, obiettivi, passaggi", priority: 1, inCharge: "Andrea", status: "pending, in progress, done" },
+          { id: 2, name: "Passport check", description: "scopo, obiettivi, passaggi", priority: 1, inCharge: "Andrea", status: "pending, in progress, done" },
+          { id: 3, name: "Hotel reservation", description: "scopo, obiettivi, passaggi", priority: 1, inCharge: "Andrea", status: "pending, in progress, done" },
+        ],
+      },
+    ],
+  };
